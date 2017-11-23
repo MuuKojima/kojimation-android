4e906cca4800f8f4b4acaaf1e5c75944b15d9cfc
muukojima <muu.kojima.dev@gmail.com>
2017-11-23 18:15:14 +0900

    first commit

---
+++ idea/import.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+# Installs IntelliJ configs into your user configs.
+
+echo "Installing Kojimation code styles..."
+
+dirs=(
+  $HOME/Library/Preferences/IntelliJIdea*
+  $HOME/Library/Preferences/IdeaIC*
+  $HOME/Library/Preferences/AndroidStudio*
+  $HOME/.IntelliJIdea*/config
+  $HOME/.IdeaIC*/config
+  $HOME/.AndroidStudio*/config
+  )
+
+# Install codestyles
+for dir in "${dirs[@]}"
+do
+  if [ -e $dir ]; then
+    if [ -d "$dir/codestyles" ]; then
+      cp -frv $( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/codestyles/* "$dir/codestyles" 2> /dev/null
+    else
+      cp -frv $( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/codestyles $dir 2> /dev/null
+    fi
+  fi
+done
+
+echo "Done."
+echo ""
+echo "Restart IntelliJ and/or AndroidStudio, go to preferences, and apply the style."
